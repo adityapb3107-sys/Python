e408cecd7da53862b6e1c60154702c6f6d793c11
Aditya Bhelave <adityapb3107@gmail.com>
2026-06-01 01:48:00 +0530

    Add files via upload

---
+++ Decoding_my_secrete_language.py
@@ -0,0 +1,9 @@
+a=input("Enter your code language: ")
+if len(a)>=9:
+    a1=a[3:-4]
+    a2=a[-4] + a2
+    print(f"Your decoded word is: {a2}")
+else:
+    a=a[::-1]
+    a=a[3:-3]
+    print(f"Your decoded word is: {a}")
